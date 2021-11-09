d86daaac62d78d75d5f110668ddd56ae65b57f07
Miller Tatiana <millertatiana@MacBook-Pro.fritz.box>
2021-11-09 18:47:33 +0100

    first commit

---
+++ string.js
@@ -0,0 +1,32 @@
+
+let result = '';
+let newStr = '';
+
+
+function isValid(str) {
+    if (str.length > 10) {
+        newStr = str.slice(0, 10);
+        console.log(newStr);
+    }
+}
+
+function toUpCase(str) {
+    if (str[0].toLowerCase()) {
+        newStr = str[0].toUpperCase();
+
+    }
+    console.log(newStr);
+}
+
+
+function showVerticalMessage(str) {
+    if (isValid() && toUpCase()) {
+        for (let i = 0; i < str.length; i++) {
+
+            result = str[i] + '\n';
+            console.log(result);
+        }
+    }
+}
+
+showVerticalMessage('marathonmmmmmmm');

207a21e4bef420187bf6d48df1cc651bd6f701c3
Miller Tatiana <millertatiana@MacBook-Pro.fritz.box>
2021-11-09 19:46:17 +0100

    Aenderungen

--- string.js
+++ string.js
@@ -4,23 +4,23 @@ let newStr = '';
 
 function isLang(str) {
     if (str.length > 10) {
-        newStr = str.slice(0, 10);
-        console.log(newStr);
+        str = str.slice(0, 10);
+        console.log(str);
     }
 }
 
 
 function toUpCase(str) {
-    if (str[0].toLowerCase()) {
-        newStr = str[0].toUpperCase() + str.slice(1);
+    if (str[0].toLowerCase() == true) {
+        str = str[0].toUpperCase() + str.slice(1);
 
     }
-    console.log(newStr);
+    console.log(str);
 }
 
 
 function showVerticalMessage(str) {
-    if (isValid && toUpCase) {
+    if (isLang(str) && toUpCase(str)) {
         for (let i = 0; i < str.length; i++) {
 
             result = str[i] + '\n';

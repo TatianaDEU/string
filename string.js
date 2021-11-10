b401a6dfa1fa41df5695f433895ec6faa7c0def8
Tatiana <millert0312@gmail.com>
2021-11-10 13:13:13 +0100

    zwei if

--- string.js
+++ string.js
@@ -4,15 +4,17 @@ let result = '';
 function showVerticalMessage(str) {
     if (str.length > 10) {
         str = str.slice(0, 10);
-    } else if (str[0].toLowerCase() === true) {
-            str = str[0].toUpperCase() + str.slice(1);
-       }
-        for (let i = 0; i < str.length; i++) {
-            
-            result = str[i] + '\n';
-            console.log(result);
+    };
+
+    if (str[0] == str[0].toLowerCase()) {
+        str = str[0].toUpperCase() + str.slice(1);
+    };
+    
+    for (let i = 0; i < str.length; i++) {
+        result = str[i] + '\n';
+        console.log(result);
     }
-   
+
 }
 
 showVerticalMessage('marathonmmmmmmm');

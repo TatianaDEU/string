b7baf004e735772f4d7d67a5f4b15fd6fc1d0622
Tatiana <millert0312@gmail.com>
2021-11-10 13:05:39 +0100

    Aenderungen

--- string.js
+++ string.js
@@ -1,33 +1,18 @@
 
 let result = '';
-let newStr = '';
-
-function isLang(str) {
-    if (str.length > 10) {
-        str = str.slice(0, 10);
-        console.log(str);
-    }
-}
-
-
-function toUpCase(str) {
-    if (str[0].toLowerCase() == true) {
-        str = str[0].toUpperCase() + str.slice(1);
-
-    }
-    console.log(str);
-}
-
 
 function showVerticalMessage(str) {
-    if (isLang(str) && toUpCase(str)) {
+    if (str.length > 10) {
+        str = str.slice(0, 10);
+    } else if (str[0].toLowerCase() === true) {
+            str = str[0].toUpperCase() + str.slice(1);
+       }
         for (let i = 0; i < str.length; i++) {
-
+            
             result = str[i] + '\n';
             console.log(result);
-        }
     }
+   
 }
 
-//toUpCase('dndndn');
 showVerticalMessage('marathonmmmmmmm');
